cb0a31aa8442bc8bb10fbf209a8fb45f9b8ddafa
Aleem Abdul <aleemsameer1@msitprogram.net>
2020-07-16 20:56:56 +0530

    manual push

--- 05-rectangle_overlap-Python/rectangle_overlap.py
+++ 05-rectangle_overlap-Python/rectangle_overlap.py
@@ -8,6 +8,12 @@
 # head down (so we say that "up is down")
 
 def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
-    return True
+    if left1 + width1 <left2:
+        return False
+    elif top1+height1>top2:
+        return False
+    else:
+        return True
+
 
         
